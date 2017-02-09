9d6b624381632ded7261d36427375d6cbc046b41
Latoya Watson <latoyamwatson@gmail.com>
2017-02-09 16:34:29 -0500

    playing with scope and declaration functions

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("i'm working!");
+
+// declaration function
+function buttonClicked() {
+  var secretWord = document.getElementById("secretWord").value
+  var wordSplit = secretWord.split("")
+  console.log(secretWord);
+  console.log(wordSplit);
+  didItWork()
+}
+
+function didItWork() {
+  // out of scope example
+  console.log(wordSplit);
+}
